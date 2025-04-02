00e060f7d86affe845e05639e596626781240033
Zachery Pipes <zacherypipes@gmail.com>
2025-04-01 21:38:08 -0500

    Used PerlinNoise.hpp to create a proper map

--- world.cpp
+++ world.cpp
@@ -1,8 +1,8 @@
-#include "world.h"
-
 #include <iostream>
 
+#include "world.h"
 #include "TextureManager.h"
+#include "PerlinNoise.hpp"
 
 World::World(const int mapWidth, const int mapHeight)
 : mapWidth(mapWidth + 2), mapHeight(mapHeight + 2) {
@@ -16,6 +16,52 @@ World::World(const int mapWidth, const int mapHeight)
         for(int x = 10; x < mapWidth; ++x) {
             if(x == 10 || y == 10 || x == mapWidth - 1 || y == mapHeight - 1) {
                 map[{x,y}] = Tile(Border, TextureManager::BorderTexture);
+            }
+        }
+    }
+
+    // Generating the world
+    const siv::PerlinNoise::seed_type seed = 121569189489843456u;
+    const siv::PerlinNoise perlin { seed };
+
+    // Values for scale calculation
+    constexpr int minVal = 1;
+    constexpr int maxVal = 7;
+    constexpr int minPerlin = 0;
+    constexpr int maxPerlin = 1;
+
+
+    for(int y = 0; y < 100; y++) {
+        for(int x = 0; x < 100; x++) {
+            const double noise = perlin.octave2D_01((x * 0.01), (y * 0.01), 6,.5);
+
+            const int scaled_noise = minVal + (maxVal - minVal) * noise;
+
+            // weighed_noise to tiles
+            switch(scaled_noise) {
+                case 1:
+                    map[{x,y}] = Tile(DeepSea, TextureManager::SeaTexture);
+                    break;
+                case 2:
+                    map[{x,y}] = Tile(Sea, TextureManager::SeaTexture);
+                    break;
+                case 3:
+                    map[{x,y}] = Tile(Shore, TextureManager::ShoreTexture);
+                    break;
+                case 4:
+                    map[{x,y}] = Tile(Beach, TextureManager::BeachTexture);
+                    break;
+                case 5:
+                    map[{x,y}] = Tile(Grass, TextureManager::GrassTexture);
+                    break;
+                case 6:
+                    map[{x,y}] = Tile(Hills, TextureManager::HillsTexture);
+                    break;
+                case 7:
+                    map[{x,y}] = Tile(Mountains, TextureManager::MountainsTexture);
+                    break;
+                default:
+                    map[{x,y}] = Tile(Border, TextureManager::BorderTexture);
             }
         }
     }
